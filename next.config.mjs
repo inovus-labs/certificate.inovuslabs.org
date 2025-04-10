f78f8de2dbac6b61d0fe3fdca1e44663ff622a53
Arjun Krishna <arjun12345krishna@gmail.com>
2025-04-10 20:21:36 +0530

    fix : Remove GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 
-const isGithubPages = process.env.GITHUB_PAGES === 'true'
-const repoName = process.env.REPO_NAME
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
-  output: 'export',
-  basePath: isGithubPages ? `/${repoName}` : '',
-  assetPrefix: isGithubPages ? `/${repoName}/` : '',
-  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
