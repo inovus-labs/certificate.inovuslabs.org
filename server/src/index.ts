f6c0339432be7ea0275ca324511af66a6c21d6dc
Arjun Krishna <arjun12345krishna@gmail.com>
2025-05-23 22:42:11 +0530

    feat : update Client Base URL for CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL || 'https://certificate.inovuslabs.org',
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
