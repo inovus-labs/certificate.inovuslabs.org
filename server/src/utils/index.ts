ca1da3941db6269176e7a60046093a3a94b62850
Arjun Krishna <arjun12345krishna@gmail.com>
2025-06-15 20:46:26 +0530

    feat : [server] - Seeder to bulk add Certificates

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -4,3 +4,11 @@ export const toISOString = (unixSeconds: any): string | null => {
     const date = new Date(Number(unixSeconds.toString()) * 1000);
     return date.toISOString();
 };
+
+
+export function SerialDateToISOString(serial: number): string {
+    const utc_days = Math.floor(serial - 25569);
+    const utc_value = utc_days * 86400; // seconds in a day
+    const date = new Date(utc_value * 1000);
+    return date.toISOString();
+}
